7bf9121fe4b84e8465e65720894c807d79dcf531
Daniel <Daniel.Lorrig@rwth-aachen.de>
2021-09-03 15:44:07 +0200

    Added CanvasWrapper

---
+++ CanvasWrapper.cs
@@ -0,0 +1,47 @@
+﻿using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Graphics;
+
+namespace OVFSliceViewer
+{
+    public class CanvasWrapper : LayerViewer.Model.ICanvas
+    {
+        GLControl _canvas;
+        public CanvasWrapper(GLControl gl)
+        {
+            _canvas = gl;
+            _canvas.Show();
+        }
+        public void Init()
+        {
+            MakeCurrent();
+            GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        }
+
+        public void SwapBuffers()
+        {
+            _canvas.SwapBuffers();
+        }
+        public void MakeCurrent()
+        {
+            _canvas.MakeCurrent();
+        }
+
+        public void Resize(System.Drawing.Size size)
+        {
+            if (_canvas.ClientSize.Height == 0)
+                _canvas.ClientSize = size;
+
+            GL.Viewport(size);
+
+        }
+        public System.Drawing.Size GetCanvasArea()
+        {
+            return _canvas.ClientSize;
+        }
+        public float Width => _canvas.Width;
+
+        public float Height => _canvas.Height;
+    }
+
+}
