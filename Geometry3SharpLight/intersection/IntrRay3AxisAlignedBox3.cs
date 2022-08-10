026f78d9f955b89686f5c454d34fadd0d6ee9a07
unknown <lukas.unruh@rwth-aachen.de>
2022-08-10 14:41:49 +0200

    Added slightly modified files from g3Sharp in a separate project g3SharpLight.

---
+++ Geometry3SharpLight/intersection/IntrRay3AxisAlignedBox3.cs
@@ -0,0 +1,36 @@
+﻿// Modified file from https://github.com/gradientspace/geometry3Sharp
+
+using OpenTK;
+
+namespace Geometry3SharpLight
+{
+    class IntrRay3AxisAlignedBox3
+    {
+        /// <summary>
+        /// Find intersection of ray with AABB, without having to construct any new classes.
+        /// Returns ray T-value of first intersection (or double.MaxVlaue on miss)
+        /// </summary>
+        public static bool FindRayIntersectT(ref Ray3f ray, ref AxisAlignedBox3f box, out double RayParam)
+        {
+            double RayParam0 = 0.0;
+            double RayParam1 = double.MaxValue;
+            int Quantity = 0;
+            Vector3 Point0 = Vector3.Zero;
+            Vector3 Point1 = Vector3.Zero;
+            IntersectionType Type = IntersectionType.Empty;
+            IntrLine3AxisAlignedBox3.DoClipping(ref RayParam0, ref RayParam1, ref ray.Origin, ref ray.Direction, ref box,
+                      true, ref Quantity, ref Point0, ref Point1, ref Type);
+
+            if (Type != IntersectionType.Empty)
+            {
+                RayParam = RayParam0;
+                return true;
+            }
+            else
+            {
+                RayParam = double.MaxValue;
+                return false;
+            }
+        }
+    }
+}
---
+++ Geometry3SharpLight/math/Ray3.cs
@@ -0,0 +1,38 @@
+﻿// Modified file from https://github.com/gradientspace/geometry3Sharp
+
+using OpenTK;
+
+namespace Geometry3SharpLight
+{
+    public struct Ray3f
+    {
+        public Vector3 Origin;
+        public Vector3 Direction;
+
+        public Ray3f(Vector3 origin, Vector3 direction, bool bIsNormalized = false)
+        {
+            this.Origin = origin;
+            this.Direction = direction;
+            if (bIsNormalized == false)
+                Direction.Normalize();
+        }
+
+        // parameter is distance along ray
+        public Vector3 PointAt(float d)
+        {
+            return Origin + d * Direction;
+        }
+
+        public float Project(Vector3 p)
+        {
+            return Vector3.Dot((p - Origin), (Direction));
+        }
+
+        public float DistanceSquared(Vector3 p)
+        {
+            float t = Vector3.Dot((p - Origin), Direction);
+            Vector3 proj = Origin + t * Direction;
+            return (proj - p).LengthSquared;
+        }
+    }
+}
