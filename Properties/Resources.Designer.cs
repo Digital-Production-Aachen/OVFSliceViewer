8e25c9e7ac773be4dc290e980bad46b941477040
Daniel <Daniel.Lorrig@rwth-aachen.de>
2023-04-05 11:14:22 +0200

    Renamed projects.

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace LayerViewer.Properties {
+namespace OVFSliceViewer.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace LayerViewer.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("LayerViewer.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("OVFSliceViewer.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
